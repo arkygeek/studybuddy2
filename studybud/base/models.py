ea99f35829a22ede353cfc7c90cf507a5221af9c
Dr. Jason Jorgenson <jjorgenson@gmail.com>
2022-02-07 15:14:12 -0700

    to 35 minutes in tutorial. this is in virtualenv

---
+++ studybud/base/models.py
@@ -0,0 +1,6 @@
+from django.db import models
+
+# Create your models here.
+"""
+This is where we are going to configure the database
+"""
---
+++ studybud/base/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+
+# Create your views here.
+"""
+Views are what get called when someone goes to a specific URL.
+These are going to be functions or classes.
+They are going to fire off things like:
+  - queries to the database
+  - any templates that need to be rendered
+  - etc
+"""
