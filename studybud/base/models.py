859c5e1df88d302d04f1273b1abcdc607e1db4db
Dr. Jason Jorgenson <jjorgenson@gmail.com>
2022-02-08 13:32:53 -0700

    just some notes as comments

--- studybud/base/models.py
+++ studybud/base/models.py
@@ -4,6 +4,9 @@ from django.db import models
 """
 This is where we are going to configure the database
 
+We are going to create Python classes.
+The classes we create represent the database tables.
+
 Python Class
 (Inherits from Django Models)
 
@@ -11,5 +14,18 @@ class Project(models.Model):
       title = models.CharField()
       description = models.TextField()
       id = models.UUIDField()
+
+The above will create a table in the database called project
+and every attribute inside of that Python class will represent a column
+inside of the database.
+
+      Project Database Table
+      ID    TITLE       DESC
+      1     ...         ...        <-- each row is like an instance of a class
+      2     ...         ...
+      3     ...         ...
+      4     ...         ...
+      5     ...         ...
+      ...
 
 """
