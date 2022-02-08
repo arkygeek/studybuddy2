c300424733dc55f6546a28180f1d15ecd1c04ea5
Dr. Jason Jorgenson <jjorgenson@gmail.com>
2022-02-08 11:40:31 -0700

    Tuesday, at 1:05:02

--- studybud/base/models.py
+++ studybud/base/models.py
@@ -3,4 +3,13 @@ from django.db import models
 # Create your models here.
 """
 This is where we are going to configure the database
+
+Python Class
+(Inherits from Django Models)
+
+class Project(models.Model):
+      title = models.CharField()
+      description = models.TextField()
+      id = models.UUIDField()
+
 """
