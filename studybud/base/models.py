6bf3866abf8966a106b21151d9ef3da90deffa39
Dr. Jason Jorgenson <jjorgenson@gmail.com>
2022-02-08 20:25:04 -0700

    some of this seems to work lol. but not in preview

--- studybud/base/models.py
+++ studybud/base/models.py
@@ -1,9 +1,10 @@
-from unittest.util import _MAX_LENGTH
-from django.db import models
-
 # Create your models here.
-"""
-This is where we are going to configure the database
+""" By default, the db cannot have an instance of something without having
+      a value for every entry, so we set null to equal true so that we _can_
+      have an empty entry here. name, for example, cannot be empty. Likewise,
+      blank form is ok.
+      """
+""" This is where we are going to configure the database
 
 We are going to create Python classes.
 The classes we create represent the database tables.
@@ -33,18 +34,46 @@ The classes are like models of the database, so that's why it's called models.py
 
 """
 
-class Room(models.Model):
-      #host =
-      #topic =
+
+from os import O_NDELAY
+from unittest.util import _MAX_LENGTH
+from django.db import models
+from django.contrib.auth.models import User
+
+
+class Topic(models.Model):
       name = models.CharField(max_length=200)
-      description = models.TextField(null=True, blank=True)
-      # by default, the db cannot have an instance of something without having
-      # a value for every entry, so we set null to equal true so that we _can_
-      # have an empty entry here. name, for example, cannot be empty. Likewise,
-      # blank form is ok.
-      #participants =
-      updated = models.DateTimeField(auto_now=True)
-      created = models.DateTimeField(auto_now_add=True)
 
       def __str__(self):
-            return str(self.name)
+          return self.name
+
+
+class Room(models.Model):
+    host = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    topic = models.ForeignKey(Topic, on_delete=models.SET_NULL, null=True)
+    name = models.CharField(max_length=200)
+    description = models.TextField(null=True, blank=True)
+    participants = models.ManyToManyField(
+        User, related_name='participants', blank=True)
+    updated = models.DateTimeField(auto_now=True)
+    created = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['-updated', '-created']
+
+    def __str__(self):
+        return self.name
+
+
+class Message(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    room = models.ForeignKey(Room, on_delete=models.CASCADE)
+    body = models.TextField()
+    updated = models.DateTimeField(auto_now=True)
+    created = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['-updated', '-created']
+
+    def __str__(self):
+        return self.body[0:50]
