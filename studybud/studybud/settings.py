5980e1d440db1874e6d13356527f54129f5aba4a
Dr. Jason Jorgenson <jjorgenson@gmail.com>
2022-02-19 10:33:26 -0700

    del own msgs in recent activity now at 3:35:49

--- studybud/studybud/settings.py
+++ studybud/studybud/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'django_extensions', # I installed this to generate UML charts
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
