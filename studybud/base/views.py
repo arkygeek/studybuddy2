2732fdf23db1ad3fc8b407df039750ccbe5c3a01
Dr. Jason Jorgenson <jjorgenson@gmail.com>
2022-02-20 16:16:55 -0700

    things seem to be working now with create room

--- studybud/base/views.py
+++ studybud/base/views.py
@@ -153,23 +153,29 @@ def userProfile(request, pk):
 @login_required(login_url='login')
 def createRoom(request):
   form = RoomForm()
-  topics = Topic.objects.all()
+  # topics = Topic.objects.all()
 
   if request.method == 'POST':
-    topic_name = request.POST.get('topic')
-    topic, created = Topic.objects.get_or_create(name=topic_name)
+    form = RoomForm(request.POST)
+    if form.is_valid():
+      room = form.save(commit=False)
+      room.host = request.user
+      room.save()
+      return redirect('home')
+    # topic_name = request.POST.get('topic')
+    # topic, created = Topic.objects.get_or_create(name=topic_name)
 
-    Room.Objects.create(
-      host=request.user,
-      topic=topic,
-      name=request.POST.get('name'),
-      description=request.POST.get('description'),
-    )
-    return redirect('home')
+    # Room.Objects.create(
+    #   host=request.user,
+    #   topic=topic,
+    #   name=request.POST.get('name'),
+    #   description=request.POST.get('description'),
+    # )
+    # return redirect('home')
 
   context = {
     'form': form,
-    'topics': topics
+    # 'topics': topics
   }
   return render(request, 'base/room_form.html', context)
 
